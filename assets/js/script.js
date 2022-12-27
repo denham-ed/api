b12d7ec888bac562a131f67c582b5cfcb9cc7e41
Ed Denham <ed.denham@gmail.com>
2022-12-27 00:48:18 +0000

    Wire and debug form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,7 @@ const resultsModal = new bootstrap.Modal(
   document.getElementById("resultsModal")
 );
 
-document
-  .getElementById("status")
-  .addEventListener("click", (e) => getStatus(e));
+document.getElementById("status").addEventListener("click", (e) => getStatus(e));
 
 document.getElementById("submit").addEventListener("click", (e) => postForm(e));
 
@@ -17,6 +15,7 @@ const getStatus = async (e) => {
   if (response.ok) {
     displayStatus(data);
   } else {
+    displayException(data);
     throw new Error(data.error);
   }
 };
@@ -30,9 +29,22 @@ const displayStatus = (data) => {
   resultsModal.show();
 };
 
-const postForm = async (e) => {
-  const form = new FormData(document.getElementById("checksform"));
+const processOptions = (form) => {
+  let optArray = [];
+  for (let entry of form.entries()) {
+    if (entry[0] === "options") {
+      optArray.push(entry[1]);
+    }
+  }
+  form.delete("options");
+  form.append("options", optArray.join());
+  return form;
+};
 
+const postForm = async (e) => {
+  const form = processOptions(
+    new FormData(document.getElementById("checksform"))
+  );
   const response = await fetch(API_URL, {
     method: "POST",
     headers: {
@@ -45,20 +57,31 @@ const postForm = async (e) => {
   if (response.ok) {
     displayErrors(data);
   } else {
+    displayException(data);
     throw new Error(data.error);
   }
 };
 
 const displayErrors = (data) => {
-    document.getElementById("resultsModalTitle").innerText = `JSHint Results for ${data.file}`;
-    let errors = ""
-    data.error_list.forEach((error) => {
-        errors += `<div>col: ${error.col} error: ${error.error}</div>`
-    })
-    document.getElementById("results-content").innerHTML = errors
+  document.getElementById(
+    "resultsModalTitle"
+  ).innerText = `JSHint Results for ${data.file}`;
+  let errors = "";
+  data.error_list.forEach((error) => {
+    errors += `<div>col: ${error.col} error: ${error.error}</div>`;
+  });
+  document.getElementById("results-content").innerHTML = errors;
 
+  resultsModal.show();
+};
 
-
-    resultsModal.show();
-
-}
+const displayException = (exception) => {
+  document.getElementById("resultsModalTitle").innerText =
+    "An Exception Occurred";
+  document.getElementById("results-content").innerHTML = `
+    <div>The API returned status code ${exception.status_code}</div>
+    <div>Error number: <span class="bold"> ${exception.error_no}</span></div>
+    <div>Error text: <span class="bold"> ${exception.error}</span></div>
+    `;
+  resultsModal.show();
+};
